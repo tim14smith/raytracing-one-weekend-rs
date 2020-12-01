0af10e02c325a77a99bf554175d8edac746cb124
Tim Smith <tim14smith@gmail.com>
2020-11-30 19:49:37 -0600

    Chapter 2

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let image_width = 256;
+    let image_height = 256;
+
+    println!("P3\n{} {}\n255", image_width, image_height);
+
+    for j1 in 0..image_height {
+        let j = image_height - j1 - 1;
+        eprintln!("\rScanlines remaining {} ", j);
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+    eprintln!("Done.\n");
+}
